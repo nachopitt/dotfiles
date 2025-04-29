02297a3e703269d6c75da23b96b163b3263d37b4
nachopitt <nachopitt@gmail.com>
2025-04-29 12:58:02 -0600

    Update PowerShell git-config alias function
    Update PowerShell git-config alias function so now it uses the yadm
    local repo instead.

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 function git-config {
-    git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME @args
+    git --git-dir=$HOME/.local/share/yadm/repo.git --work-tree=$HOME @args
 }
 
 Set-Alias -Name config -Value git-config
